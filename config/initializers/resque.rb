aca81b526d451d0324d428f1b72574f8ab67dcad
TanTanMa <gelatan12@gmail.com>
2022-01-14 22:16:03 +0800

    heroku redis url update

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,6 +4,6 @@ require 'uri'
 if Rails.env.development? or Rails.env.test?
   Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else
-  uri = URI.parse(ENV.fetch("REDIS_URL"))
+  uri = URI.parse(ENV.fetch("HEROKU_REDIS_YELLOW_URL"))
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
