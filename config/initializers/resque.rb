c3ff286e5dd5abe8fabe526bb75d7825f207025e
TanTanMa <gelatan12@gmail.com>
2022-01-14 20:52:27 +0800

    added redis url to env for production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,6 +4,6 @@ require 'uri'
 if Rails.env.development? or Rails.env.test?
   Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else
-  uri = URI.parse(Rails.application.credentials.redis_heroku[:REDIS_URL])
+  uri = URI.parse(ENV.fetch("REDIS_URL"))
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
